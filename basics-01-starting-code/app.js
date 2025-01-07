1fe2440d96f0b8684e722aa173f964c1cbf05b3d
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-07 16:52:35 +0600

    v-html

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -2,9 +2,17 @@ const app = Vue.createApp({
     // data: function(){} is same as data(){}
     data() {
         return {
-            courseGoal: "Finish the course and learn Vue!",
+            courseGoalA: "<h1>Finish the course and learn Vue</h1>",
+            courseGoalB: "<h3>Master the course</h3>",
             vueLink: 'https://vuejs.org'
         };
+    },
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) return this.courseGoalA;
+            else return this.courseGoalB;
+        }
     }
 });
 app.mount('#user-goal');
