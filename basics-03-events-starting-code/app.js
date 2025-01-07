58b89bdfd408904b7f9a649ec0b1ea29aa19c421
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-07 19:12:52 +0600

    $event vs auto event

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,11 +2,13 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '?'
     };
   },
   methods: {
     add(num = 1) { this.counter += num; },
-    reduce() { this.counter--; }
+    reduce() { this.counter--; },
+    setName(event, lastname) { this.name = event.target.value + ' ' + lastname; }
   }
 });
 
