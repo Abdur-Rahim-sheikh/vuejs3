3fa0257626309926e3e4fad5133870850fec0406
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-07 19:00:41 +0600

    v-on

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -4,6 +4,10 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
+  methods: {
+    add(num = 1) { this.counter += num; },
+    reduce() { this.counter--; }
+  }
 });
 
 app.mount('#events');
