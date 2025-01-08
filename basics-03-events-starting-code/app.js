ea01d24d5e83cbbd7dc8449635dc2df5a04c0b97
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-08 14:24:22 +0600

    v-model two way binding

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,20 +2,17 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 1,
-      name: '?',
-      confirmedName: ''
+      name: '',
     };
   },
   methods: {
-    confirmedInput() {
-      this.confirmedName = this.name;
-    },
+
     add(num = 1) { this.counter += num; },
     reduce() { this.counter--; },
-    setName(event, lastname) { this.name = event.target.value + ' ' + lastname; },
-    submitForm() {
-      alert("Submitted!")
-    }
+    setName(event, lastname) { this.name = event.target.value; },
+    resetInput() {
+      this.name = '';
+    },
   }
 });
 
