1d7cb933285e1bd6b2cab306507d7ef2bd73d64e
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-08 14:59:27 +0600

    computed

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -5,11 +5,18 @@ const app = Vue.createApp({
       name: '',
     };
   },
+  computed: {
+    fullname() {
+      console.log("Running....")
+      let tem = this.name
+      if (this.name) tem += ' Sheikh'
+      return tem;
+    },
+  },
   methods: {
-
     add(num = 1) { this.counter += num; },
     reduce() { this.counter--; },
-    setName(event, lastname) { this.name = event.target.value; },
+    setName(event) { this.name = event.target.value; },
     resetInput() {
       this.name = '';
     },
