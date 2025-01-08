933825162151eb033332988df608071063022b27
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-08 15:21:57 +0600

    watcher

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -3,15 +3,32 @@ const app = Vue.createApp({
     return {
       counter: 1,
       name: '',
+      fullnameCount: 0
     };
   },
   computed: {
+    // name computed property as data variable
     fullname() {
       console.log("Running....")
       let tem = this.name
       if (this.name) tem += ' Sheikh'
       return tem;
     },
+  },
+  watch: {
+    // this will watch data and computed variables
+    // each can fetch nothing or currvalue or both cur and old value
+    fullname() {
+      this.fullnameCount++;
+      console.log('watching fullname', this.fullnameCount);
+    },
+    counter(value) {
+      console.log(value);
+    },
+    name(value, newvalue) {
+      console.log(value, newvalue);
+      this.fullname = value;
+    }
   },
   methods: {
     add(num = 1) { this.counter += num; },
