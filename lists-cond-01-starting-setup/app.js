2c2f123a17f56468bbecd00d852969f2577e50d8
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-10 19:58:47 +0600

    bug introduced:
    when we delete in v-for it tries to be optimum and just moves the below content to upward causing issue

--- lists-cond-01-starting-setup/app.js
+++ lists-cond-01-starting-setup/app.js
@@ -7,6 +7,9 @@ const app = Vue.createApp({
     addGoal() {
       this.goals.push(this.enteredGoalValue);
       this.enteredGoalValue = '';
+    },
+    removeGoal(index) {
+      this.goals.splice(index, 1);
     }
   }
 });
