9867e6af8b845f5a84c76d9bda7ed3f3deef922e
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-13 19:16:08 +0600

    ref="unique_id"
    it's a attribute in html but only known to vue, it somewhat works like id and we can extraction the whole element from the script like this.$refs.unique_id.
    
    It helps us to use value when we need rather than every keystroke and handling the whole element is easy.

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -10,7 +10,8 @@ const app = Vue.createApp({
       this.currentUserInput = event.target.value;
     },
     setText() {
-      this.message = this.currentUserInput;
+      // this.message = this.currentUserInput;
+      this.message = this.$refs.inputField.value;
     },
   },
 });
@@ -18,23 +19,23 @@ const app = Vue.createApp({
 app.mount('#app');
 
 
-const data = {
-  message: 'Hello!',
-  longMessage: 'Hello! World!'
-};
-const handler = {
-  set(target, key, value) {
-    console.log(target, key, value);
-    if (key === 'message') {
-      target.longMessage = value + ' World!';
-    }
-    target.message = value;
-  }
-};
-const proxy = new Proxy(data, handler);
+// const data = {
+//   message: 'Hello!',
+//   longMessage: 'Hello! World!'
+// };
+// const handler = {
+//   set(target, key, value) {
+//     console.log(target, key, value);
+//     if (key === 'message') {
+//       target.longMessage = value + ' World!';
+//     }
+//     target.message = value;
+//   }
+// };
+// const proxy = new Proxy(data, handler);
 
-proxy.message = 'Hello abir!!!';
-console.log(proxy.longMessage);
+// proxy.message = 'Hello abir!!!';
+// console.log(proxy.longMessage);
 
-proxy.message = 'kutria';
-console.log(proxy.longMessage);
+// proxy.message = 'kutria';
+// console.log(proxy.longMessage);
