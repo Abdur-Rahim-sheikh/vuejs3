de4f6fadab955f87ebef1fd9f3fed5735185e12c
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-17 16:54:55 +0600

    form data emitted

---
+++ vue-cli-01-a-new-vue-project/src/components/NewFriend.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+    emits: ['addFriend'],
+    data() {
+        return {
+            name: '',
+            phone: '',
+            email: ''
+        }
+    },
+    methods: {
+        submitFriendData() {
+            this.$emit('addFriend', {
+                name: this.name,
+                phone: this.phone,
+                email: this.email
+            });
+        }
+    }
+};
+</script>
+
+<template>
+    <form @submit.prevent="submitFriendData">
+        <div>
+            <label>Name</label>
+            <input v-model="name" type="text" placeholder="Name" />
+        </div>
+        <div>
+            <label>Phone</label>
+            <input v-model="phone" type=" tel" placeholder="Phone" />
+        </div>
+        <div>
+            <label>Email</label>
+            <input v-model="email" type="email" placeholder="Email" />
+        </div>
+        <div>
+            <button type="submit">Add Friend</button>
+        </div>
+
+    </form>
+</template>
