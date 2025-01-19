da0b99d3c5d9fa3fb53b99e40ab625c58b0147c3
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-19 15:00:19 +0600

    teleport - to
    it's a vue special tag. When we have a pop up element which is general hidden but triggers for specific task it helps us to move the element on surface level rather then in a nested place

--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,7 +1,41 @@
+<script>
+import ErrorAlert from './ErrorAlert.vue'
+export default {
+    components: {
+        ErrorAlert
+    },
+    data() {
+        return {
+            invalidInput: false
+        }
+    },
+    methods: {
+        setGoals() {
+            let value = this.$refs.goal.value;
+            if (value === '') {
+                // alert('Input must not be empty')
+                this.invalidInput = true;
+            }
+        },
+        confirmError() {
+            this.invalidInput = false;
+        }
+    }
+}
+</script>
+
 <template>
     <div>
         <h2>Manage Goals</h2>
-        <input type="text">
+        <input type="text" ref="goal">
+        <button @click="setGoals">Set Goal</button>
+        <Teleport to="body">
+            <ErrorAlert v-if="invalidInput">
+                <h2>Input Is invalid!</h2>
+                <p>Please enter at least one character</p>
+                <button @click="confirmError">Okay</button>
+            </ErrorAlert>
+        </Teleport>
     </div>
 
 </template>
