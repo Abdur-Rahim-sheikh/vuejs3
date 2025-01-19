1f28e2f308e6eb82033d95f9aa390674b54e1113
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-19 19:27:43 +0600

    listing

---
+++ 10-learning-resources-app/src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import ResourceItem from './components/resources/ResouceItem.vue'
+export default {
+    components: {
+        ResourceItem,
+    },
+    data() {
+        return {
+            storedResources: [
+                {
+                    id: 'official-guide',
+                    title: 'Official Guide',
+                    description: 'The official vue js documentations.',
+                    link: 'https://vuejs.org'
+                },
+                {
+                    id: 'google',
+                    title: 'Google',
+                    description: 'Your online friend',
+                    link: 'https://google.com'
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <ul>
+        <ResourceItem :resource="resource" v-for="resource in storedResources" :key="resource.id"></ResourceItem>
+    </ul>
+</template>
---
+++ 10-learning-resources-app/src/components/resources/ResouceItem.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    props: ["resource"]
+}
+</script>
+
+<template>
+    <li>
+        <div>
+            <header>
+                <h3>{{ resource.title }}</h3>
+                <button>Delete</button>
+            </header>
+        </div>
+        <p>{{ resource.description }}</p>
+        <nav>
+            <a :href="resource.link">View Resource</a>
+        </nav>
+    </li>
+</template>
