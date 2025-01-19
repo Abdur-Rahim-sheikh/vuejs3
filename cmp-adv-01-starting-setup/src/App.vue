ee414bb314d24a6419512c49e0acdb988bd019f0
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-19 13:24:04 +0600

    dynamic component
    there is a component tag with `is` attribute which helps us dynamically bind component without explicitly defininging them nor using lot's of if-else

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -1,18 +1,19 @@
 <script>
 import TheHeader from './components/TheHeader.vue'
-import BadgeList from './components/BadgeList.vue'
-import UserInfo from './components/UserInfo.vue'
-import CourseGoals from './components/CourseGoals.vue'
-
+// import BadgeList from './components/BadgeList.vue'
+// import UserInfo from './components/UserInfo.vue'
+// import CourseGoals from './components/CourseGoals.vue'
+import ActiveGoals from './components/ActiveGoals'
+import ManageGoals from './components/ManageGoals'
 export default {
   components: {
     TheHeader,
-    BadgeList,
-    UserInfo,
-    CourseGoals
+    ActiveGoals,
+    ManageGoals
   },
   data() {
     return {
+      selectedComponent: 'ActiveGoals',
       activeUser: {
         name: 'Maximilian Schwarzmüller',
         description: 'Site owner and admin',
@@ -20,20 +21,22 @@ export default {
       },
     };
   },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    }
+  }
 };
 </script>
 
 <template>
   <div>
     <TheHeader />
-    <BadgeList />
-    <UserInfo :full-name="activeUser.name" :info-text="activeUser.description" :role="activeUser.role" />
-    <CourseGoals>
-      <template #default="slotProps">
-        <h2>{{ slotProps.item }}</h2>
-        <p>{{ slotProps['anotherValue'] }}</p>
-      </template>
-    </CourseGoals>
+    <button @click="setSelectedComponent('ActiveGoals')">Active Goals</button>
+    <button @click="setSelectedComponent('ManageGoals')">Manage Goals </button>
+    <!-- <ActiveGoals v-if="selectedComponent === 'ActiveGoals'" />
+    <ManageGoals v-if="selectedComponent === 'ManageGoals'" /> -->
+    <component :is="selectedComponent"></component>
   </div>
 </template>
 
