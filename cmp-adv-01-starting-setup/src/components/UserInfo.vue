0a30796514567ebe87a4951f801ba81ee6f47149
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-19 10:55:59 +0600

    v-slot:default
    if we keep one slot unnamed we can address that as default, it also help use reduce error, as where we use this slot component no astray code can be left behind it will give error

--- cmp-adv-01-starting-setup/src/components/UserInfo.vue
+++ cmp-adv-01-starting-setup/src/components/UserInfo.vue
@@ -5,7 +5,10 @@
         <h3>{{ fullName }}</h3>
         <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
       </template>
-      <p>{{ infoText }}</p>
+      <template v-slot:default>
+        <p>{{ infoText }}</p>
+      </template>
+
     </BaseCard>
   </section>
 </template>
