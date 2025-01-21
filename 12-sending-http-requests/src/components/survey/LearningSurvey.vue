e7689f9880f89292cd182ebf827e0435db0e8fdf
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-21 20:11:40 +0600

    edge case handled if no data yet

--- 12-sending-http-requests/src/components/survey/LearningSurvey.vue
+++ 12-sending-http-requests/src/components/survey/LearningSurvey.vue
@@ -1,34 +1,3 @@
-<template>
-  <section>
-    <base-card>
-      <h2>How was you learning experience?</h2>
-      <form @submit.prevent="submitSurvey">
-        <div class="form-control">
-          <label for="name">Your Name</label>
-          <input type="text" id="name" name="name" v-model.trim="enteredName" />
-        </div>
-        <h3>My learning experience was ...</h3>
-        <div class="form-control">
-          <input type="radio" id="rating-poor" value="poor" name="rating" v-model="chosenRating" />
-          <label for="rating-poor">Poor</label>
-        </div>
-        <div class="form-control">
-          <input type="radio" id="rating-average" value="average" name="rating" v-model="chosenRating" />
-          <label for="rating-average">Average</label>
-        </div>
-        <div class="form-control">
-          <input type="radio" id="rating-great" value="great" name="rating" v-model="chosenRating" />
-          <label for="rating-great">Great</label>
-        </div>
-        <p v-if="invalidInput">One or more input fields are invalid. Please check your provided data.</p>
-        <div>
-          <base-button>Submit</base-button>
-        </div>
-      </form>
-    </base-card>
-  </section>
-</template>
-
 <script>
 export default {
   data() {
@@ -38,6 +7,7 @@ export default {
       invalidInput: false,
       url: 'https://vue-http-demo-b6b20-default-rtdb.asia-southeast1.firebasedatabase.app/',
       endpoint: 'survey.json',
+
     };
   },
   // emits: ['survey-submit'],
@@ -69,6 +39,39 @@ export default {
   },
 };
 </script>
+
+<template>
+  <section>
+    <base-card>
+      <h2>How was you learning experience?</h2>
+      <form @submit.prevent="submitSurvey">
+        <div class="form-control">
+          <label for="name">Your Name</label>
+          <input type="text" id="name" name="name" v-model.trim="enteredName" />
+        </div>
+        <h3>My learning experience was ...</h3>
+        <div class="form-control">
+          <input type="radio" id="rating-poor" value="poor" name="rating" v-model="chosenRating" />
+          <label for="rating-poor">Poor</label>
+        </div>
+        <div class="form-control">
+          <input type="radio" id="rating-average" value="average" name="rating" v-model="chosenRating" />
+          <label for="rating-average">Average</label>
+        </div>
+        <div class="form-control">
+          <input type="radio" id="rating-great" value="great" name="rating" v-model="chosenRating" />
+          <label for="rating-great">Great</label>
+        </div>
+        <p v-if="invalidInput">One or more input fields are invalid. Please check your provided data.</p>
+        <div>
+          <base-button>Submit</base-button>
+        </div>
+      </form>
+    </base-card>
+  </section>
+</template>
+
+
 
 <style scoped>
 .form-control {
--- 12-sending-http-requests/src/components/survey/UserExperiences.vue
+++ 12-sending-http-requests/src/components/survey/UserExperiences.vue
@@ -1,18 +1,3 @@
-<template>
-  <section>
-    <base-card>
-      <h2>Submitted Experiences</h2>
-      <div>
-        <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
-      </div>
-      <ul>
-        <survey-result v-for="result in results" :key="result.id" :name="result.name"
-          :rating="result.rating"></survey-result>
-      </ul>
-    </base-card>
-  </section>
-</template>
-
 <script>
 import SurveyResult from './SurveyResult.vue';
 
@@ -26,14 +11,17 @@ export default {
       results: [],
       url: 'https://vue-http-demo-b6b20-default-rtdb.asia-southeast1.firebasedatabase.app/',
       endpoint: 'survey.json',
+      isLoading: true,
     };
   },
   methods: {
     loadExperiences() {
+      this.isLoading = true;
       fetch(`${this.url}/${this.endpoint}`)
         .then(response => response.json())
         .then(data => {
           const results = [];
+
           for (const key in data) {
             const result = {
               id: key,
@@ -42,12 +30,40 @@ export default {
             };
             results.push(result);
           }
+
           this.results = results;
+          this.isLoading = false;
+
         });
+      // we cannot use here as it will not wait for the fetch to complete
+      // this.isLoading = false;
     },
-  }
+  },
+  mounted() {
+    this.loadExperiences();
+  },
 };
 </script>
+
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
+      </div>
+      <p v-if="isLoading">Loading...</p>
+      <ul v-else-if="results.length > 0">
+        <SurveyResult v-for="result in results" :key="result.id" :name="result.name" :rating="result.rating">
+        </SurveyResult>
+      </ul>
+      <p v-else>No experiences submitted yet!</p>
+
+    </base-card>
+  </section>
+</template>
+
+
 
 <style scoped>
 ul {
