e96dafa3ea42648040766771e7a442272de76251
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-21 12:59:18 +0600

    teleport gives it's charm
    just define teleport to body and wherever you write it, it will teleport to body section out of the nesting and we can define the telport inside the dialog component itself

--- 10-learning-resources-app/src/components/UI/BaseDialog.vue
+++ 10-learning-resources-app/src/components/UI/BaseDialog.vue
@@ -12,22 +12,24 @@ export default {
 </script>
 
 <template>
-  <div @click="$emit('close')"></div>
-  <dialog open>
-    <header>
-      <slot name="header">
-        <h2>{{ title }}</h2>
-      </slot>
-    </header>
-    <section>
-      <slot></slot>
-    </section>
-    <menu>
-      <slot name="actions">
-        <button @click="$emit('close')">Close</button>
-      </slot>
-    </menu>
-  </dialog>
+  <Teleport to="body">
+    <div @click="$emit('close')"></div>
+    <dialog open>
+      <header>
+        <slot name="header">
+          <h2>{{ title }}</h2>
+        </slot>
+      </header>
+      <section>
+        <slot></slot>
+      </section>
+      <menu>
+        <slot name="actions">
+          <button @click="$emit('close')">Close</button>
+        </slot>
+      </menu>
+    </dialog>
+  </Teleport>
 </template>
 
 <style scoped>
