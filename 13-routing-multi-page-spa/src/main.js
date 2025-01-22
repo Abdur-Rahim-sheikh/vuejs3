de283a6a8f24c5e91bc61bab461cee772b211fdf
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-22 16:56:29 +0600

    organizing routes

--- 13-routing-multi-page-spa/src/main.js
+++ 13-routing-multi-page-spa/src/main.js
@@ -1,71 +1,7 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue';
-import TeamsList from './components/teams/TeamsList.vue'
-import UsersList from './components/users/UsersList.vue'
-import TeamMembers from './components/teams/TeamMembers.vue'
-import NotFound from './components/nav/NotFound.vue'
-import TeamsFooter from './components/teams/TeamsFooter.vue'
-import UsersFooter from './components/users/UsersFooter.vue'
+import router from './router.js';
 
-const router = createRouter({
-    routes: [
-        { path: '/', redirect: '/teams' },
-        {
-            name: 'teams',
-            path: '/teams',
-            meta: { needsAuth: true },
-            // component: TeamsList,
-            components: {
-                default: TeamsList,
-                footer: TeamsFooter
-            },
-            children: [
-                { name: "team-members", path: ':teamId', component: TeamMembers, props: true }
-            ]
-        },
-        {
-            name: "users",
-            path: '/users',
-            components: {
-                default: UsersList, footer: UsersFooter
-            },
-            beforeEnter(to, from, next) {
-                console.log('users beforeEnter')
-                console.log(to, from)
-                next()
-            }
-        },
-
-        // { path: '/:notFound(.*)', redirect: '/teams' }
-        { path: '/:notFound(.*)', component: NotFound }
-    ],
-    scrollBehavior(_, _2, savedPosition) {
-        // console.log(to, from, savedPosition)
-        if (savedPosition) {
-            return savedPosition
-        }
-        return { left: 0, top: 0 }
-    },
-    history: createWebHistory()
-})
-
-router.beforeEach(function (to, from, next) {
-    console.log('Global beforeEach')
-    console.log(to, from)
-    // if (to.name === 'team-members') {
-    //     next()
-    // } else {
-    //     next({ name: 'team-members', params: { teamId: 't2' } })
-    // }
-    console.log('needs auth', to.name, to.meta.needsAuth)
-    next()
-})
-
-router.afterEach(function (to, from) {
-    console.log('Global afterEach')
-    console.log(to, from)
-})
 
 const app = createApp(App)
 app.use(router);
