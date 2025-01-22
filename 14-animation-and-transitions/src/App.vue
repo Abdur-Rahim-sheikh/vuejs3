557071e5307c740fdc859fcf6e6bb562e992ce69
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-22 17:32:18 +0600

    animation

--- 14-animation-and-transitions/src/App.vue
+++ 14-animation-and-transitions/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
-    <div class="block"></div>
-    <button>Animate</button>
+    <div class="block" :class="{ animate: blockAnimated }"></div>
+    <button @click="animateBlock">Animate</button>
   </div>
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
     <p>This is a test dialog!</p>
@@ -10,12 +10,15 @@
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
   </div>
-</template>  
+</template>
 
 <script>
 export default {
   data() {
-    return { dialogIsVisible: false };
+    return {
+      dialogIsVisible: false,
+      blockAnimated: false
+    };
   },
   methods: {
     showDialog() {
@@ -24,6 +27,12 @@ export default {
     hideDialog() {
       this.dialogIsVisible = false;
     },
+    animateBlock() {
+      this.blockAnimated = true;
+      // setTimeout(() => {
+      //   this.blockAnimated = false;
+      // }, 300);
+    }
   },
 };
 </script>
@@ -32,12 +41,15 @@ export default {
 * {
   box-sizing: border-box;
 }
+
 html {
   font-family: sans-serif;
 }
+
 body {
   margin: 0;
 }
+
 button {
   font: inherit;
   padding: 0.5rem 2rem;
@@ -47,17 +59,22 @@ button {
   color: white;
   cursor: pointer;
 }
+
 button:hover,
 button:active {
   background-color: #a80b48;
   border-color: #a80b48;
 }
+
 .block {
   width: 8rem;
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
+  /* transition: transform 0.3s ease-out; */
+
 }
+
 .container {
   max-width: 40rem;
   margin: 2rem auto;
@@ -68,5 +85,24 @@ button:active {
   padding: 2rem;
   border: 2px solid #ccc;
   border-radius: 12px;
+}
+
+.animate {
+  /* transform: translateX(-50px); */
+  animation: slideFade 0.3s ease-out forwards;
+}
+
+@keyframes slideFade {
+  0% {
+    transform: translateX(0) scale(1);
+  }
+
+  70% {
+    transform: translateX(-50px) scale(1.2);
+  }
+
+  100% {
+    transform: translateX(-80px) scale(1);
+  }
 }
 </style>
