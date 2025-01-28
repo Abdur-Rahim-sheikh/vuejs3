241234f52042ae2f1aee397a12fedfa89ec00f7b
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-28 16:33:10 +0600

    all component initialized

---
+++ 16-main_project_coach_webapp/src/pages/coaches/CoachDetail.vue
@@ -0,0 +1,8 @@
+<script>
+
+</script>
+
+<template>Details for coach
+    <RouterView />
+    <RouterLink to="/coaches/placeholder/contact">Contact</RouterLink>
+</template>
--- 16-main_project_coach_webapp/src/router.js
+++ 16-main_project_coach_webapp/src/router.js
@@ -3,7 +3,7 @@ import CoachDetail from './pages/coaches/CoachDetail.vue'
 import CoachesList from './pages/coaches/CoachesList.vue'
 import CoachRegistration from './pages/coaches/CoachRegistration.vue'
 import ContactCoach from './pages/requests/ContactCoach.vue'
-import RequestsReceived from './pages/requests/RequestsReceived.vue'
+import RequestsReceived from './pages/requests/RequestsRecieved.vue'
 import NotFound from './pages/NotFound.vue'
 const router = createRouter({
     history: createWebHistory(),
@@ -12,7 +12,7 @@ const router = createRouter({
         { path: '/coaches', component: CoachesList },
         {
             path: '/coaches/:id', component: CoachDetail, children: [
-                { path: '/contact', component: ContactCoach }
+                { path: 'contact', component: ContactCoach }
             ]
         },
         { path: '/register', component: CoachRegistration },
