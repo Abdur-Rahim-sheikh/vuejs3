602e2b4a14b8806522f729b2e19f27bf82955e0f
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-29 20:05:41 +0600

    form built

---
+++ 16-main_project_coach_webapp/src/components/coaches/CoachForm.vue
@@ -0,0 +1,119 @@
+<script>
+export default {
+    data() {
+        return {
+            firstname: '',
+            lastname: '',
+            description: '',
+            rate: '',
+            areas: []
+        }
+    },
+    methods: {
+        submitForm() {
+            const coachData = {
+                firstname: this.firstname,
+                lastname: this.lastname,
+                description: this.description,
+                rate: this.rate,
+                areas: this.areas
+            }
+            console.log(coachData);
+        }
+    }
+}
+</script>
+
+<template>
+    <form @submit.prevent="submitForm">
+        <div class="form-control">
+            <label for="firstname">First Name</label>
+            <input v-model.trim="firstname" type="text" id="firstname" />
+        </div>
+        <div class="form-control">
+            <label for="lastname">Last Name</label>
+            <input v-model.trim="lastname" type="text" id="lastname" />
+        </div>
+        <div class="form-control">
+            <label for="description">Description</label>
+            <textarea v-model.trim="description" id="description" rows="5"></textarea>
+        </div>
+        <div class="form-control">
+            <label for="rate">Hourly Rate</label>
+            <input v-model.number="rate" type="number" id="rate" />
+        </div>
+        <div class="form-control">
+            <h3>Areas of Expertise</h3>
+            <div>
+                <input v-model="areas" type="checkbox" id="frontend" value="frontend">
+                <label for="frontend">Frontend Development</label>
+            </div>
+            <div>
+                <input v-model="areas" type="checkbox" id="backend" value="backend">
+                <label for="backend">Backend Development</label>
+            </div>
+            <div>
+                <input v-model="areas" type="checkbox" id="career" value="career">
+                <label for="career">Career Advisory</label>
+            </div>
+        </div>
+        <BaseButton>Register</BaseButton>
+    </form>
+</template>
+
+<style scoped>
+.form-control {
+    margin: 0.5rem 0;
+}
+
+label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+}
+
+input[type='checkbox']+label {
+    font-weight: normal;
+    display: inline;
+    margin: 0 0 0 0.5rem;
+}
+
+input,
+textarea {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+}
+
+input:focus,
+textarea:focus {
+    background-color: #f0e6fd;
+    outline: none;
+    border-color: #3d008d;
+}
+
+input[type='checkbox'] {
+    display: inline;
+    width: auto;
+    border: none;
+}
+
+input[type='checkbox']:focus {
+    outline: #3d008d solid 1px;
+}
+
+h3 {
+    margin: 0.5rem 0;
+    font-size: 1rem;
+}
+
+.invalid label {
+    color: red;
+}
+
+.invalid input,
+.invalid textarea {
+    border: 1px solid red;
+}
+</style>
--- 16-main_project_coach_webapp/src/pages/coaches/CoachRegistration.vue
+++ 16-main_project_coach_webapp/src/pages/coaches/CoachRegistration.vue
@@ -1,1 +1,17 @@
-<template>Register</template>
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+export default {
+    components: {
+        CoachForm
+    }
+}
+</script>
+
+<template>
+    <section>
+        <BaseCard>
+            <h2>Register as a coach now</h2>
+            <CoachForm />
+        </BaseCard>
+    </section>
+</template>
