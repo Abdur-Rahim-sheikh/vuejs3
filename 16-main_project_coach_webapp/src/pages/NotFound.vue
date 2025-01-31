72405abfdc0f49e2564c9b6f3697297a635e62eb
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-31 18:41:19 +0600

    not found

--- 16-main_project_coach_webapp/src/pages/NotFound.vue
+++ 16-main_project_coach_webapp/src/pages/NotFound.vue
@@ -1,3 +1,9 @@
 <template>
-    <p>Not Found</p>
+    <section>
+        <BaseCard>
+            <h2>Page not found</h2>
+            <p>This page could not be found, goto <RouterLink to="/">homepage</RouterLink>
+            </p>
+        </BaseCard>
+    </section>
 </template>
