edf97dea6acc54c0f515a9913583362086df463e
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-31 15:50:12 +0600

    requests to firebase

--- 16-main_project_coach_webapp/src/pages/requests/RequestsRecieved.vue
+++ 16-main_project_coach_webapp/src/pages/requests/RequestsRecieved.vue
@@ -1,12 +1,30 @@
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters, mapActions } from 'vuex';
 import RequestItem from '@/components/requests/RequestItem.vue';
 export default {
     components: {
         RequestItem
     },
+    data() {
+        return {
+            isLoading: false,
+            error: null,
+        }
+    },
     computed: {
-        ...mapGetters('requests', ['requests', 'hasRequests'])
+        ...mapGetters('requests', ['requests', 'hasRequests']),
+    },
+    methods: {
+        ...mapActions('requests', ['loadRequests']),
+    },
+    async created() {
+        this.isLoading = true;
+        try {
+            await this.loadRequests();
+        } catch (error) {
+            this.error = error.message || 'Failed to load requests';
+        }
+        this.isLoading = false;
     }
 }
 </script>
@@ -18,12 +36,18 @@ export default {
                 <h2>Requests Received</h2>
             </header>
         </BaseCard>
-        <ul v-if="hasRequests">
+        <ul v-if="isLoading">
+            <BaseSpinner />
+        </ul>
+        <ul v-else-if="hasRequests">
             <RequestItem v-for="request in requests" :key="request.id" :email="request.userEmail"
                 :message="request.userMessage" />
         </ul>
         <h3 v-else>You haven't received any requests yet!</h3>
     </section>
+    <BaseDialog :show="!!error" @close="error = null">
+        <p>{{ error }}</p>
+    </BaseDialog>
 </template>
 
 <style scoped>
--- 16-main_project_coach_webapp/src/store/requests.js
+++ 16-main_project_coach_webapp/src/store/requests.js
@@ -1,3 +1,5 @@
+const firebaseUrl = process.env.VUE_APP_FIREBASE_URL;
+
 export default {
     namespaced: true,
     state() {
@@ -11,19 +13,48 @@ export default {
         }
     },
     actions: {
-        addRequest(context, request) {
+        async addRequest(context, request) {
+
             const data = {
-                id: new Date().toISOString(),
-                coachId: request.coachId,
                 userEmail: request.email,
                 userMessage: request.message,
             }
+            let url = `${firebaseUrl}/requests/${request.coachId}.json`;
+            let response = await fetch(url, {
+                method: 'POST',
+                body: JSON.stringify(data)
+            });
+            let responseData = await response.json();
+            if (!response.ok) {
+                const error = new Error(responseData.message || 'Failed to send request.');
+                throw error;
+            }
             context.commit('addRequest', data);
-        }
+        },
+        async loadRequests(context) {
+            const coachId = context.rootGetters.userId;
+            let url = `${firebaseUrl}/requests/${coachId}.json`;
+            let response = await fetch(url);
+            let responseData = await response.json();
+            if (!response.ok) {
+                const error = new Error(responseData.message || 'Failed to fetch requests.');
+                throw error;
+            }
+            const requests = [];
+            for (const key in responseData) {
+                const request = {
+                    id: key,
+                    userEmail: responseData[key].userEmail,
+                    userMessage: responseData[key].userMessage,
+                };
+                requests.push(request);
+            }
+            context.state.requests = requests
+        },
     },
     getters: {
-        requests(state, _, _2, rootGetters) {
-            return state.requests.filter(req => req.coachId === rootGetters.userId);
+        requests(state,) {
+            return state.requests
         },
         hasRequests(_, getters) {
             return getters.requests.length > 0;
