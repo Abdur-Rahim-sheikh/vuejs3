6201fc4d01d2e63577acd1f6f424a0c1962a265b
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-02-02 20:41:08 +0600

    asynchronous import
    Well vue has a method called `defineAsyncComponent` which helps to load component lazyly.
    
    At the same time if we want to load routes lazyly it gives even more suitable way just make it function call and it will load only when it's first needed.
    ie
    ```js
    const ContactCoach = () => import('./pages/requests/ContactCoach.vue')
    ```

--- 16-main_project_coach_webapp/src/router.js
+++ 16-main_project_coach_webapp/src/router.js
@@ -1,13 +1,16 @@
+// import { defineAsyncComponent } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import CoachDetail from './pages/coaches/CoachDetail.vue'
 import CoachesList from './pages/coaches/CoachesList.vue'
-import CoachRegistration from './pages/coaches/CoachRegistration.vue'
-import ContactCoach from './pages/requests/ContactCoach.vue'
-import RequestsReceived from './pages/requests/RequestsRecieved.vue'
-import NotFound from './pages/NotFound.vue'
-import UserAuth from './pages/auth/UserAuth.vue'
-
 import store from './store/index.js'
+
+// router does not definceAsyncComponent to make it work.
+const CoachDetail = () => import('./pages/coaches/CoachDetail.vue')
+const CoachRegistration = () => import('./pages/coaches/CoachRegistration.vue')
+const NotFound = () => import('./pages/NotFound.vue')
+const RequestsReceived = () => import('./pages/requests/RequestsReceived.vue')
+const UserAuth = () => import('./pages/auth/UserAuth.vue')
+const ContactCoach = () => import('./pages/requests/ContactCoach.vue')
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
