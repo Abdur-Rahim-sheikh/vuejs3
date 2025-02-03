955ea02c5061c41eadd0db0f8075375b78959fac
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-02-03 13:09:16 +0600

    refactor: project componets from options to composition

--- 19-composition_api_project/src/components/projects/ProjectItem.vue
+++ 19-composition_api_project/src/components/projects/ProjectItem.vue
@@ -1,14 +1,14 @@
+<script setup>
+defineProps(['title']);
+</script>
+
 <template>
   <li>
     <h3>{{ title }}</h3>
   </li>
 </template>
 
-<script>
-export default {
-  props: ['title'],
-};
-</script>
+
 
 <style scoped>
 li {
--- 19-composition_api_project/src/components/projects/ProjectsList.vue
+++ 19-composition_api_project/src/components/projects/ProjectsList.vue
@@ -1,3 +1,82 @@
+<script setup>
+import { watch, computed, defineProps, ref } from 'vue';
+import ProjectItem from './ProjectItem.vue';
+
+const props = defineProps(['user']);
+const enteredSearchTerm = ref('');
+const activeSearchTerm = ref('');
+
+console.log(props)
+const availableProjects = computed(() => {
+  if (activeSearchTerm.value) {
+    return props.user.projects.filter((prj) =>
+      prj.title.includes(activeSearchTerm.value)
+    );
+  }
+  return props.user.projects;
+});
+
+const hasProjects = computed(() => {
+  return props.user.projects && availableProjects.value.length > 0;
+});
+
+
+
+watch(enteredSearchTerm, (val) => {
+  setTimeout(() => {
+    if (val === enteredSearchTerm.value) {
+      activeSearchTerm.value = val;
+    }
+  }, 300);
+});
+const updateSearch = (val) => {
+  enteredSearchTerm.value = val;
+};
+
+// export default {
+//   components: {
+//     ProjectItem,
+//   },
+//   props: ['user'],
+//   data() {
+//     return {
+//       enteredSearchTerm: '',
+//       activeSearchTerm: '',
+//     };
+//   },
+//   computed: {
+//     hasProjects() {
+//       return this.user.projects && this.availableProjects.length > 0;
+//     },
+//     availableProjects() {
+//       if (this.activeSearchTerm) {
+//         return this.user.projects.filter((prj) =>
+//           prj.title.includes(this.activeSearchTerm)
+//         );
+//       }
+//       return this.user.projects;
+//     },
+//   },
+//   methods: {
+//     updateSearch(val) {
+//       this.enteredSearchTerm = val;
+//     },
+//   },
+//   watch: {
+//     enteredSearchTerm(val) {
+//       setTimeout(() => {
+//         if (val === this.enteredSearchTerm) {
+//           this.activeSearchTerm = val;
+//         }
+//       }, 300);
+//     },
+//     user() {
+//       this.enteredSearchTerm = '';
+//     },
+//   },
+// };
+</script>
+
 <template>
   <base-container v-if="user">
     <h2>{{ user.fullName }}: Projects</h2>
@@ -12,52 +91,7 @@
   </base-container>
 </template>
 
-<script>
-import ProjectItem from './ProjectItem.vue';
 
-export default {
-  components: {
-    ProjectItem,
-  },
-  props: ['user'],
-  data() {
-    return {
-      enteredSearchTerm: '',
-      activeSearchTerm: '',
-    };
-  },
-  computed: {
-    hasProjects() {
-      return this.user.projects && this.availableProjects.length > 0;
-    },
-    availableProjects() {
-      if (this.activeSearchTerm) {
-        return this.user.projects.filter((prj) =>
-          prj.title.includes(this.activeSearchTerm)
-        );
-      }
-      return this.user.projects;
-    },
-  },
-  methods: {
-    updateSearch(val) {
-      this.enteredSearchTerm = val;
-    },
-  },
-  watch: {
-    enteredSearchTerm(val) {
-      setTimeout(() => {
-        if (val === this.enteredSearchTerm) {
-          this.activeSearchTerm = val;
-        }
-      }, 300);
-    },
-    user() {
-      this.enteredSearchTerm = '';
-    },
-  },
-};
-</script>
 
 <style scoped>
 ul {
