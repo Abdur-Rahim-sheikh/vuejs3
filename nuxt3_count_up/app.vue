44e9109fd96d920c78ad1bf44a21c15d70617242
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-02-28 23:43:02 +0600

    data passing correctly

--- nuxt3_count_up/app.vue
+++ nuxt3_count_up/app.vue
@@ -1,6 +1,43 @@
+<script setup>
+const table = useState('table', () => [])
+const generate = (m) => {
+  const data = reactive([])
+  for (let i = 0; i < m; i++) {
+
+    data.push([
+      i,
+      Math.floor(Math.random() * 100),
+      Math.floor(Math.random() * 100),
+      Math.floor(Math.random() * 100)
+    ])
+  }
+  return data
+}
+const increment = (table) => {
+  let x = Math.floor(Math.random() * table.value.length)
+  let y = Math.floor(Math.random() * (table.value[0].length - 1)) + 1
+  table.value[x][y] += Math.floor(Math.random() * 10)
+
+  console.log(x, y, table.value[x][y])
+}
+
+
+console.log(table.value)
+let id = null;
+onMounted(() => {
+  table.value = generate(3)
+  id = setInterval(() => {
+    increment(table)
+  }, 5 * 1000)
+})
+
+onUnmounted(() => {
+  clearInterval(id)
+})
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <RenderTable :table="table" />
   </div>
 </template>
---
+++ nuxt3_count_up/components/RenderTable.vue
@@ -0,0 +1,26 @@
+<script setup>
+const { table } = defineProps(['table'])
+
+</script>
+
+<template>
+    <span v-if="table.length === 0">No data provided</span>
+    <div v-else>
+        <table>
+            <tr>
+                <th>id</th>
+                <th>age</th>
+                <th>weight</th>
+                <th>height</th>
+            </tr>
+
+            <tr v-for="row in table" :key="row[0]">
+                <td>{{ row[0] }}</td>
+                <td>{{ row[1] }}</td>
+                <td>{{ row[2] }}</td>
+                <td>{{ row[3] }}</td>
+            </tr>
+
+        </table>
+    </div>
+</template>
